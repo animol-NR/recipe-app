004773bb73d07bdc12ec5032151eba13456248b8
Animol N R <animol698@gmail.com>
2020-07-01 09:43:03 +0530

    Updates

---
+++ precache-manifest.ffab60cfab162a6728b15c5128837d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120dadd70d6df8469f52a9f26028086e",
+    "url": "/animol-NR/recipe-app/index.html"
+  },
+  {
+    "revision": "90e593dede050bca691e",
+    "url": "/animol-NR/recipe-app/static/css/main.6f306ce0.chunk.css"
+  },
+  {
+    "revision": "3773e44d1196f65021b2",
+    "url": "/animol-NR/recipe-app/static/js/2.0002be96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/animol-NR/recipe-app/static/js/2.0002be96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e593dede050bca691e",
+    "url": "/animol-NR/recipe-app/static/js/main.95d8ff2d.chunk.js"
+  },
+  {
+    "revision": "e4bc5a8dfcd670571d06",
+    "url": "/animol-NR/recipe-app/static/js/runtime-main.36d0f544.js"
+  }
+]);
